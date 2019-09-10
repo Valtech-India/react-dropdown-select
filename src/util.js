07e089403b7a6a06d2a0c84d6fbd8f84607f4b9f
Rakesh-Harihar <32760380+Rakesh-Harihar@users.noreply.github.com>
2019-09-10 12:11:16 +0530

    updated isEqual method
    updated isEqual method to check with only value filed

--- src/util.js
+++ src/util.js
@@ -1,48 +1,57 @@
-export const valueExistInSelected = (value, values, props) =>
-  !!values.find((val) => getByPath(val, props['valueField']) === value);
-
-export const hexToRGBA = (hex, alpha) => {
-  const RR = parseInt(hex.slice(1, 3), 16);
-  const GG = parseInt(hex.slice(3, 5), 16);
-  const BB = parseInt(hex.slice(5, 7), 16);
-
-  return `rgba(${RR}, ${GG}, ${BB}${alpha && `, ${alpha}`})`;
+(exports.__esModule = !0),
+(exports.getProp = exports.getByPath = exports.isEqual = exports.debounce = exports.hexToRGBA = exports.valueExistInSelected = void 0);
+const valueExistInSelected = function(a, b, c) {
+  return !!b.find(b => {
+    return getByPath(b, c.valueField) === a;
+  });
 };
+exports.valueExistInSelected = valueExistInSelected;
+const hexToRGBA = function(a, b) {
+  const c = parseInt(a.slice(1, 3), 16);
 
-export const debounce = (fn, delay = 0) => {
-  let timerId;
+  const d = parseInt(a.slice(3, 5), 16);
 
-  return (...args) => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-    timerId = setTimeout(() => {
-      fn(...args);
-      timerId = null;
-    }, delay);
+  const e = parseInt(a.slice(5, 7), 16);
+  return `rgba(${c}, ${d}, ${e}${b && `, ${b}`})`;
+};
+exports.hexToRGBA = hexToRGBA;
+const debounce = function(a, b) {
+  void 0 === b && (b = 0);
+  let c;
+  return function() {
+    for (var d = arguments.length, e = Array(d), f = 0; f < d; f++) e[ f ] = arguments[ f ];
+    c && clearTimeout(c),
+    (c = setTimeout(() => {
+      a(...e), (c = null);
+    }, b));
   };
 };
-
-export const isEqual = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-
-export const getByPath = (object, path) => {
-  if (!path) {
-    return;
-  }
-
-  return path.split('.').reduce((acc, value) => acc[value], object)
+exports.debounce = debounce;
+const isEqual = function(c, a) {
+const objc = {
+    value: c[ 0 ].value
+}
+  const obja = {
+    value: a[ 0 ].value
+  };
+  return JSON.stringify(objc) === JSON.stringify(obja);
 };
-
-export const getProp = (object, path, defaultValue) => {
-  if (!path) {
-    return object;
-  }
-
-  const normalizedPath = Array.isArray(path) ? path : path.split('.').filter((item) => item.length);
-
-  if (!normalizedPath.length) {
-    return object === undefined ? defaultValue : object;
-  }
-
-  return getProp(object[normalizedPath.shift()], normalizedPath, defaultValue);
+exports.isEqual = isEqual;
+var getByPath = function(a, b) {
+  return b
+    ? b.split(".").reduce((a, b) => {
+      return a[ b ];
+    }, a)
+    : void 0;
 };
+exports.getByPath = getByPath;
+var getProp = function(a, b, c) {
+  if (!b) return a;
+  const d = Array.isArray(b)
+    ? b
+    : b.split(".").filter(a => {
+      return a.length;
+    });
+  return d.length ? getProp(a[ d.shift() ], d, c) : void 0 === a ? c : a;
+};
+exports.getProp = getProp;
